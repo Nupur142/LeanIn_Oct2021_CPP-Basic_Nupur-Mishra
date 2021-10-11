640ad12ac92a9f85ba2890158b881918f82779ce
Nupur142 <91134572+Nupur142@users.noreply.github.com>
2021-10-11 16:17:31 +0530

    Q 2

---
+++ Print.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+ 
+ int main()
+ {
+     int x;
+     cout<< "Enter a number ";
+     cin>>x;
+     cout<<"Number entered by the user"<<endl<<x;
+     return 0;
+ }
+
+ /*Enter a number 123
+Number entered by the user
+123*/
