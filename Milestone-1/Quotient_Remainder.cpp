29e72fd449c7e8ab2c695d22e22e66f830b9c728
Nupur142 <91134572+Nupur142@users.noreply.github.com>
2021-10-12 18:05:03 +0530

    Q 5

---
+++ Milestone-1/Quotient_Remainder.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+
+using namespace std;
+ 
+ int main()
+ {
+     int a,b;
+     cout<< "Enter a number ";
+     cin>>a;
+     cout<< "Enter another number ";
+     cin>>b;
+     cout<<"Quotient of the numbers "<<endl<<a/b<<endl; 
+     cout<<"Remaider of the Numbers "<<endl<<a%b<<endl;
+     return 0;
+ }
+ /*Enter a number 5
+Enter another number 2
+Quotient of the numbers 
+2
+Remaider of the Numbers
+1*/
